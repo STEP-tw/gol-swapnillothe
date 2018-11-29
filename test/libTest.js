544658fcbf4b0dd039dec89d62ee08f416030f4f
lswapnil <swapnillothe15@gmail.com>
2018-11-29 16:57:43 +0530

    Add some test cases for getNextGeneration function.

--- test/libTest.js
+++ test/libTest.js
@@ -106,10 +106,17 @@ describe('getNextGeneration',() => {
 });
 
 describe("getNthGeneration",function() {
-    let currentGeneration = [[0,1],[0,2],[0,3]];
-    let expectedGen = []
     let bounds = {topLeft: [1,1], bottomRight: [3,3]};
   it("should work for current generation",function() {
+    let currentGeneration = [[0,1],[0,2],[0,3]];
     deepEqual( getNthGeneration( currentGeneration, bounds, 0 ),currentGeneration);
+  });
+  it("should work for one next generation",function() {
+    let currentGeneration = [[1,1],[1,2],[1,3]];
+    deepEqual( getNthGeneration( currentGeneration, bounds, 1 ),[[1,2],[2,2]]);
+  });
+  it("should work for more than one next generation",function() {
+    let currentGeneration = [[1,1],[1,2],[1,3],[3,2]];
+    deepEqual( getNthGeneration( currentGeneration, bounds, 2 ),[[1,2],[2,2]]);
   });
 });
