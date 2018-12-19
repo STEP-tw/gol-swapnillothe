28d63a00f24b42b4ba01060a7ec3fe7d5c27756a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-19 18:09:29 +0000

    Deploy STEP-tw/gol-swapnillothe to github.com/STEP-tw/gol-swapnillothe.git:gh-pages

---
+++ precache-manifest.7364f71465780df300f51c9ae930ba35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8fd61bd83d338d508405",
+    "url": "/gol-swapnillothe/static/js/runtime~main.8fd61bd8.js"
+  },
+  {
+    "revision": "6cea87ca4fa763484f28",
+    "url": "/gol-swapnillothe/static/js/main.6cea87ca.chunk.js"
+  },
+  {
+    "revision": "126daef6fd1b7fc63351",
+    "url": "/gol-swapnillothe/static/js/1.126daef6.chunk.js"
+  },
+  {
+    "revision": "6cea87ca4fa763484f28",
+    "url": "/gol-swapnillothe/static/css/main.481766d9.chunk.css"
+  },
+  {
+    "revision": "d1016b289c60fb3ffb50946945c6a8ff",
+    "url": "/gol-swapnillothe/index.html"
+  }
+];
